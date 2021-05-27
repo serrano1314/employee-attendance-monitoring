3875721a7388eb7ec1e50f847f54cce874911ffd
Steven Serrano <steven.serrano@tup.edu.ph>
2021-05-28 00:09:15 +0800

    change table look by using treeview

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Imports
 from sqlite3.dbapi2 import Row
 from tkinter import *
-from tkinter import messagebox
+from tkinter import messagebox, ttk
 from PIL import ImageTk, Image
 from datetime import datetime, date
 import sqlite3
@@ -516,12 +516,17 @@ class Main:
                     'update_id': update_id
                 })
 
+            selected = self.tree.focus()
+            self.tree.item(selected,text='',values=(employee_id_edit.get(),first_name_edit.get(),
+                                                    last_name_edit.get(),sex_edit.get(),
+                                                    schedule_in_edit.get(),schedule_out_edit.get()))
+
             self.connection.commit()
             print('success')
             edit_page.destroy()
 
 
-    def edit_record(self,page,record_id):
+    def edit_record(self,page):
         cell_size = 35
         table_col=3
         win_size='400x300'
@@ -561,7 +566,11 @@ class Main:
         schedule_out_edit = Entry(edit_record_page, width=cell_size)
         schedule_out_edit.grid(row=8, column=table_col)
 
-        self.c.execute(f"SELECT * FROM employees WHERE employee_id = '{record_id}'")
+        record_tree = self.tree.selection()[0]
+        record = self.tree.item(record_tree)
+        id = record['values'][0]
+
+        self.c.execute(f"SELECT * FROM employees WHERE employee_id = '{id}'")
         records=self.c.fetchall()
         
         for record in records:
@@ -574,69 +583,74 @@ class Main:
             schedule_out_edit.insert(0, record[6])
 
         
-        Button(edit_record_page,text="SAVE",width=15,command=lambda id=record_id:self.update_query(edit_record_page,id),bg='#AAFE92').grid(row=9,column=table_col,pady=20)
+        Button(edit_record_page,text="SAVE",width=15,command=lambda:self.update_query(edit_record_page,id),bg='#AAFE92').grid(row=9,column=table_col,pady=20)
         Button(edit_record_page,text="CANCEL",width=15,command=lambda:edit_record_page.destroy(),bg='#FE9292').grid(row=9,column=table_col-1,pady=20,padx=(35,0))
         edit_record_page.mainloop()
 
-    def delete_query(self,page,record_id, root):
-        response = messagebox.askyesno('DELETE RECORD',f'ARE YOU SURE TO DELETE RECORD FOR {record_id} ?')
+    def delete_query(self,page, root):
+
+        record_tree = self.tree.selection()[0]
+        record = self.tree.item(record_tree)
+        id = record['values'][0]
+        response = messagebox.askyesno('DELETE RECORD',f'Delete Record for [{id}] ?')
         if(response):
-            self.c.execute(f"DELETE FROM employees WHERE employee_id = '{record_id}'")
-            self.connection.commit()         
-            page.destroy()
-            self.show_records(root)
+            print()
+            self.tree.delete(record_tree)
+            self.c.execute(f"DELETE FROM employees WHERE employee_id = '{id}'")
+            self.connection.commit()
 
     def show_records(self, root_page):
-        cell_size = 22
-        th_font ='Arial 12 bold'
+        cell_size = 165
+        mid_cell_size = 25
         win_size='1100x600'
-        table_bg_color = '#ffffff'
         show_record_page = Toplevel()
         show_record_page.title('SHOW RECORDS')
         show_record_page.geometry(win_size)
         show_record_page.config(bg=BGCOLOR)
-        
+        show_record_frame=Frame(show_record_page)
+        show_record_frame.pack(pady=10)
+
+        #for table's scrollbar
+        scrollbary = Scrollbar(show_record_frame, orient=VERTICAL)
+
+        self.tree = ttk.Treeview(show_record_frame,selectmode=BROWSE,yscrollcommand=scrollbary.set,height=20)
+        scrollbary.config(command=self.tree.yview)
+        scrollbary.pack(side=RIGHT, fill=Y)
+        self.tree['columns'] = ('EMPLOYEE ID', 'FIRST NAME', 'LAST NAME', 'SEX', 'SCHEDULE IN','SCHEDULE OUT')
+        self.tree.column('#0',width=0,stretch=NO)
+        self.tree.column('EMPLOYEE ID',width=cell_size,minwidth=mid_cell_size,anchor=W)
+        self.tree.column('FIRST NAME',width=cell_size,minwidth=mid_cell_size,anchor=W)
+        self.tree.column('LAST NAME',width=cell_size,minwidth=mid_cell_size,anchor=W)
+        self.tree.column('SEX',width=cell_size,minwidth=mid_cell_size,anchor=CENTER)
+        self.tree.column('SCHEDULE IN',width=cell_size,minwidth=mid_cell_size,anchor=CENTER)
+        self.tree.column('SCHEDULE OUT',width=cell_size,minwidth=mid_cell_size,anchor=CENTER)
+
+        self.tree.heading('#0',text='')
+        self.tree.heading('EMPLOYEE ID',text='EMPLOYEE ID',anchor=CENTER)
+        self.tree.heading('FIRST NAME',text='FIRST NAME',anchor=CENTER)
+        self.tree.heading('LAST NAME',text='LAST NAME',anchor=CENTER)
+        self.tree.heading('SEX',text='SEX',anchor=CENTER)
+        self.tree.heading('SCHEDULE IN',text='SCHEDULE IN',anchor=CENTER)
+        self.tree.heading('SCHEDULE OUT',text='SCHEDULE OUT',anchor=CENTER)
+
+       
         #select all employees except for the admin
         self.c.execute("SELECT * FROM employees WHERE employee_id  != 'admin'")
-        records = self.c.fetchall()
-
-        Label(show_record_page,bg=HEADER_COLOR,padx=500,pady=25,text="SHOW RECORDS").grid(row=4,column=5,columnspan=8)
-        table_row=5
-        
-        Label(show_record_page,bg=BGCOLOR,text="EMPLOYEE ID",font=th_font,pady=10).grid(row=table_row,column=5)
-        Label(show_record_page,bg=BGCOLOR,text="FIRST NAME ",font=th_font,pady=10).grid(row=table_row,column=6)
-        Label(show_record_page,bg=BGCOLOR,text="LAST NAME ",font=th_font,pady=10).grid(row=table_row,column=7)
-        Label(show_record_page,bg=BGCOLOR,text="SEX",font=th_font,pady=10).grid(row=table_row,column=8)
-        Label(show_record_page,bg=BGCOLOR,text="SCHEDULE IN",font=th_font,pady=10).grid(row=table_row,column=9)
-        Label(show_record_page,bg=BGCOLOR,text="SCHEDULE OUT",font=th_font,pady=10).grid(row=table_row,column=10)
-        
+        records = self.c.fetchall()        
+        #insert data
+        count=0
         for record in records:
-            stud_id=Label(show_record_page,text=record[0],width=cell_size,bg=table_bg_color,padx=0)
-            stud_id.grid(row=table_row+1,column=5)
+            self.tree.insert(parent='',index='end',iid=count,text='',
+            values=(record[0],record[2],record[3],record[4],record[5],record[6]))
+            count+=1
 
-            first_name=Label(show_record_page,text=record[2],width=cell_size,bg=table_bg_color)
-            first_name.grid(row=table_row+1,column=6)
+        self.tree.pack()
 
-            middle_name=Label(show_record_page,text=record[3],width=cell_size,bg=table_bg_color)
-            middle_name.grid(row=table_row+1,column=7)
-
-            last_name=Label(show_record_page,text=record[4],width=cell_size,bg=table_bg_color)
-            last_name.grid(row=table_row+1,column=8)
-
-            course=Label(show_record_page,text=record[5],width=cell_size,bg=table_bg_color)
-            course.grid(row=table_row+1,column=9)
-
-            section=Label(show_record_page,text=record[6],width=cell_size,bg=table_bg_color)
-            section.grid(row=table_row+1,column=10)
-
-            Button(show_record_page,text="EDIT",command=lambda edit_id=record[0]: self.edit_record(show_record_page,edit_id),bg='#AAFE92').grid(row=table_row+1,column=11)
-            Button(show_record_page,text="DELETE",command=lambda del_id=record[0]: self.delete_query(show_record_page,del_id, root_page),bg='#FE9292').grid(row=table_row+1,column=12)
-
-            table_row+=1
-
-        self.connection.commit()
-
-        Button(show_record_page, text='Cancel', width=10, command=lambda: [show_record_page.destroy(), root_page.deiconify()]).grid(row=table_row+1, column=5, pady=20)
+        edit_btn = Button(show_record_page,text="EDIT",command=lambda: self.edit_record(show_record_page),bg='#AAFE92')
+        edit_btn.pack()
+        delete_btn=Button(show_record_page,text="DELETE",command=lambda: self.delete_query(show_record_page, root_page),bg='#FE9292')
+        delete_btn.pack(pady=20)
+        Button(show_record_page, text='Go Back', width=10, command=lambda: [show_record_page.destroy(), root_page.deiconify()]).pack()
 
         # check if user Exit the Window Manually
         show_record_page.protocol("WM_DELETE_WINDOW", lambda: [show_record_page.destroy(), root_page.deiconify()])
