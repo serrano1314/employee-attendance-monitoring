50a5c6652002cf167d4335289edf733168a81cd1
Steven Serrano <steven.serrano@tup.edu.ph>
2021-06-03 04:48:05 +0800

    fix time input validation

--- admin/add_record.py
+++ admin/add_record.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 from tkinter import messagebox
 
 from config_var import *
@@ -38,37 +39,57 @@ def add_record(self, menu_page):
     self.last_name.grid(row=3, column=2, pady=(10, 0), columnspan=2)
 
     # Sex
+    sex_options = ['MALE','FEMALE']
     Label(add_record_frame, text='SEX: ', bg=BGCOLOR).grid(sticky='W',row=4, column=1, pady=(10, 0), padx=10)
-    self.sex.set('MALE')
-    option = OptionMenu(add_record_frame, self.sex, 'MALE', 'FEMALE')
-    option.grid(sticky='W',row=4, column=2, pady=(10, 0), columnspan=2)
+    self.sex = ttk.Combobox(add_record_frame,value=sex_options,width=10,state="readonly",)
+    self.sex.grid(sticky='W',row=4, column=2, pady=(10, 0), columnspan=2)
+    self.sex.current(0)
 
     # Schedule
     Label(add_record_frame, text='SCHEDULE', bg=BGCOLOR, width=10).grid(row=5, column=2, pady=20, columnspan=2)
 
+
+    hr_options = ['1','2','3','4','5','6','7','8','9','11','12']
+    min_options = ['00','15','30','45']
+    period_options = ['AM','PM']
+
     # IN
-    Label(add_record_frame, text='IN:', bg=BGCOLOR).grid(sticky='W', row=6, column=1)
-    self.schedule_in = Entry(add_record_frame, width=10)
-    self.schedule_in.grid(row=6, column=2, sticky='W')
-    morning.set('AM')
-    option_morning = OptionMenu(add_record_frame, morning, 'AM', 'PM')
-    option_morning.grid(sticky='W', row=6, column=3, pady=(10, 0))
+    in_row = 6
+    Label(add_record_frame, text='IN:', bg=BGCOLOR).grid(sticky='W', row=in_row, column=1)
+    self.schedule_in_hr = ttk.Combobox(add_record_frame, value=hr_options, width=3,state="readonly",background='#ffffff')
+    self.schedule_in_hr.current(0)
+    self.schedule_in_hr.grid(row=in_row, column=2, sticky='W')
+    Label(add_record_frame,text=":",bg=BGCOLOR).grid(row=in_row, column=2,padx=2)
+    self.schedule_in_min = ttk.Combobox(add_record_frame, value=min_options, width=3)
+    self.schedule_in_min.current(0)
+    self.schedule_in_min.grid(row=in_row, column=2, sticky='E')
+
+    self.in_period = ttk.Combobox(add_record_frame, value=period_options,width=5,state="readonly")
+    self.in_period.current(0)
+    self.in_period.grid(sticky='W', row=6, column=3,padx=10)
 
 
     # Out
-    Label(add_record_frame, text='OUT:', bg=BGCOLOR).grid(sticky='W', row=7, column=1)
-    self.schedule_out = Entry(add_record_frame, width=10)
-    self.schedule_out.grid(sticky='W', row=7, column=2)
-    night.set('PM')
-    option_night = OptionMenu(add_record_frame, night, 'AM', 'PM')
-    option_night.grid(sticky='W', row=7, column=3, pady=(10, 0))
+    out_row = in_row+1
+    Label(add_record_frame, text='OUT:', bg=BGCOLOR).grid(sticky='W', row=out_row, column=1,pady=10)
+    self.schedule_out_hr = ttk.Combobox(add_record_frame, value=hr_options, width=3,state="readonly")
+    self.schedule_out_hr.current(0)
+    self.schedule_out_hr.grid(row=out_row, column=2, sticky='W')
+    Label(add_record_frame,text=":",bg=BGCOLOR).grid(row=out_row, column=2,padx=2)
+    self.schedule_out_min = ttk.Combobox(add_record_frame, value=min_options, width=3)
+    self.schedule_out_min.current(0)
+    self.schedule_out_min.grid(row=out_row, column=2, sticky='E')
+    
+    self.out_period = ttk.Combobox(add_record_frame, value=period_options,width=5,state="readonly")
+    self.out_period.current(0)
+    self.out_period.grid(sticky='W', row=out_row, column=3,padx=10)
 
 
     # Cancel Button
-    Button(add_record_frame, text='Cancel', width=10,  bg='#e4bad4', command=lambda: [add_record_page.destroy(), menu_page.deiconify()]).grid(row=8, column=2, pady=(20, 0))
+    Button(add_record_frame, image=self.cancel_img, bd=0, bg=BGCOLOR, command=lambda: [add_record_page.destroy(), menu_page.deiconify()]).grid(row=8, column=1, pady=35)
 
     # Save Button
-    Button(add_record_frame, text='Save', bg='#AAFE92', width=10, command=lambda: [adding_record(self,add_record_page, menu_page)]).grid(row=8, column=3, pady=(20, 0))
+    Button(add_record_frame, image=self.save_img, bd=0, bg=BGCOLOR, command=lambda: [adding_record(self,add_record_page, menu_page)]).grid(row=8, column=3, pady=35)
 
     # check if user Exit the Window Manually
     add_record_page.protocol("WM_DELETE_WINDOW", lambda: [add_record_page.destroy(), menu_page.deiconify()])
@@ -85,23 +106,34 @@ def unique_id(self, id):
 def adding_record(self, adding_page, menu):
     # adding records to the database
 
-    #validation for time format, can't explain further XD
-    if ':' in self.schedule_in.get():
-        self.schedule_in = f"{self.schedule_in.get()} {morning.get()}"
-    else:
-        self.schedule_in = f"{self.schedule_in.get()}:00 {morning.get()}"
+    try:
+        in_min=int(self.schedule_in_min.get())
+        out_min=int(self.schedule_out_min.get())
+        if(in_min>=60 or out_min>=60):
+            messagebox.showerror('INVALID','Invalid Minute! \nMust less than 60')
+            return
 
-    if ':' in self.schedule_out.get():
-        self.schedule_out = f"{self.schedule_out.get()} {night.get()}"
-    else:
-        self.schedule_out = f"{self.schedule_out.get()}:00 {night.get()}"
+    except:
+        messagebox.showerror('INVALID','Invalid Minute! \nMust contain only numbers')
+        return
 
-    if len(self.schedule_in) < 8:
-        self.schedule_in = '0'+self.schedule_in
+    # if(not (self.schedule_in_min.get()<60 or self.schedule_in_min.get().isdecimal())):
+    #     messagebox.showerror('INVALID','Invalid Minute! \nMust contain only numbers and less than 60')
+    #     return
 
-    if len(self.schedule_out) < 8:
-        self.schedule_out = '0'+self.schedule_out
-        
+    x=''
+    if(len(self.schedule_in_hr.get())==1):
+        x='0'
+
+    self.schedule_in = f'{x}{self.schedule_in_hr.get()}:{self.schedule_in_min.get()} {self.in_period.get()}'
+
+    x=''
+    if(len(self.schedule_out_hr.get())==1):
+        x='0'
+
+    self.schedule_out = f'{x}{self.schedule_out_hr.get()}:{self.schedule_out_min.get()} {self.out_period.get()}'
+      
+
     if unique_id(self,self.employee_id):
         self.c.execute(f"""INSERT INTO employees VALUES(
         '{self.employee_id.get()}',
@@ -113,9 +145,12 @@ def adding_record(self, adding_page, menu):
         '{self.schedule_out}'
         )""")
         # Apply Changes to the Database
+        
         self.connection.commit()
-        adding_page.destroy()
-        menu.deiconify()
+        response = messagebox.askyesno('Successfully added in the Database','Success! Register Another Employee?')
+        if(not response):
+            adding_page.destroy()
+            menu.deiconify()
     else:
         messagebox.showerror('Error', 'Student Id Already Exist or Invalid Input')
         
