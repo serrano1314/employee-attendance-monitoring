b2ec27def4aaaf04d17efae97992d7275722b90b
Steven Serrano <steven.serrano@tup.edu.ph>
2021-06-13 06:58:45 +0800

    fix bug where checkbox in hide password not resetting

--- admin/show_record.py
+++ admin/show_record.py
@@ -78,7 +78,9 @@ def edit_record(self):
 
     password_edit['show'] = '•'
 
-    check_box = Checkbutton(edit_record_frame,command=lambda :hides(record[1], check_box),bg=BGCOLOR)
+    CheckVar = BooleanVar()
+
+    check_box = Checkbutton(edit_record_frame,command=lambda :hides(record[1], check_box),variable=CheckVar,bg=BGCOLOR)
     check_box.grid(row=3, column=table_col,sticky=E)
     
     Label(edit_record_frame,text="FIRST NAME:",bg=BGCOLOR).grid(row=4,column=table_col-1)
